a8033a90ba1cc9c39cb8d548fc5256bf529cccdd
Ahmed Gamil <ahmedgamil2010@gmail.com>
2024-11-25 17:43:50 +0200

    yes

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author gemy
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void printHelloWorld() {
+        System.out.print("Hello World");
+      }
+    
+}
