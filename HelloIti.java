6942995fdcd80845cc71025a7e151518f5e13119
jdev <hanymmme@gmail.com>
2024-11-28 10:45:43 +0200

    first commit

---
+++ HelloIti.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author gemy
+ */
+public class HelloIti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void printHelloIti() {
+        System.out.print("Hello Iti");
+      }
+    
+}
